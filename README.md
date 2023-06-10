b5beb42035deb08301c2b966ce677234864feed2
Lucas Correa <lucas.silva.correa07@aluno.ifce.edu.br>
2023-06-10 19:23:18 -0300

    adicionando projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# interface_responsiva_2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/lucascorrea/dev/interface_responsiva_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lucascorrea.dev.interface_responsiva_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
